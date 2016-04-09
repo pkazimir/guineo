69e8c3c64fc9cf247e7f7db1014bbbaa0ff303bd
Pavol Kazimir <palo.kazimir@gmail.com>
2016-04-08 22:54:30 -0400

    Added new Guineo logo

--- application/views/menu.php
+++ application/views/menu.php
@@ -6,7 +6,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span> 
         </button>
-        <span class="navbar-brand">Guineo</span>
+        <span class="navbar-brand"><img src="img/guineo-logo.png" width="150px" class="logo-img"></span>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
         <ul class="nav navbar-nav">
@@ -15,7 +15,7 @@
             <li <?php if ($screen=='add') echo 'class="active active-color"'; ?>><a href="/guineo/nuevo"><span class="glyphicon glyphicon-plus"></span> Nuevo</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right hidden-xs hidden-sm">
-          <li><a><span class="glyphicon glyphicon-user"></span> <?php echo $username; ?>@brugal.com.do</a></li>
+          <li <?php if ($screen=='profile') echo 'class="active active-color"'; ?>><a href="/guineo/perfil"><span class="glyphicon glyphicon-user"></span> <?php echo $username; ?>@brugal.com.do</a></li>
         </ul>
     </div>
   </div>
